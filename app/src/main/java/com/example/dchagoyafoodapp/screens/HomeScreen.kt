05388e7f414f1f5542642a39da0741b7dfeb0dd4
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-03-09 00:38:40 -0600

    creando homescreen

--- app/src/main/java/com/example/dchagoyafoodapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/dchagoyafoodapp/screens/HomeScreen.kt
@@ -1,2 +1,34 @@
 package com.example.dchagoyafoodapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+
+    } // fin column
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen()
+} // fin preview
