2c10f6db01c50b0dc0774c4d946dff8c164c2b08
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-03-09 02:22:49 -0600

    perzonalizacion

--- app/src/main/java/com/example/dchagoyafoodapp/models/Restaurant.kt
+++ app/src/main/java/com/example/dchagoyafoodapp/models/Restaurant.kt
@@ -11,4 +11,6 @@ val RestaurantsList = listOf(
     Restaurant("Pizza Hut", "https://www.freepnglogos.com/uploads/pizza-hut-png-logo/pizza-hut-brands-png-logo-8.png"),
     Restaurant("Fenotti", "https://images.rappi.com.mx/restaurants_logo/9599be45-63a7-4ed7-bb6b-7fc170571b30-1722910991156.png?e=webp&d=10x10&q=10"),
     Restaurant("Sushi Roll", "https://lh5.googleusercontent.com/proxy/JAZk8puv_0uoNRZgMNiEXouGHpA-FcOscKUaGQDyt_gVtD_wvcYMEebdEJ9VkG2qoser39crrGph4HE31idV_whaRZM"),
+    Restaurant("Dairy Queen ", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Dairy_Queen_logo.svg/1200px-Dairy_Queen_logo.svg.png"),
+
     )
